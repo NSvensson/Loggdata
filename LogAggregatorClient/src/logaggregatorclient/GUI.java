9c0a9bebe0e0d67f1065fb489309a5c5aa092afe
Maxarn <max.arnstrom@gmail.com>
2016-09-26 13:30:16 +0200

    Added gui_login code

--- LogAggregatorClient/src/logaggregatorclient/GUI.java
+++ LogAggregatorClient/src/logaggregatorclient/GUI.java
@@ -2,6 +2,7 @@ package logaggregatorclient;
 
 import java.util.List;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -26,13 +27,20 @@ public class GUI extends Application {
         
         Maybe a login window or something similar could be created here.
         */
-        GridPane grid = new GridPane();
+        
+        primaryStage.setScene(login(primaryStage));
+        primaryStage.setTitle("Login");
+        primaryStage.show();
+    }
+    
+    private Scene login(Stage primaryStage){
+    GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
         
-        Text login_title = new Text("Test");
+        Text login_title = new Text("Hej och välkommen");
         login_title.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         grid.add(login_title, 0, 0, 2, 1);
         
@@ -51,16 +59,33 @@ public class GUI extends Application {
         login_button_hbox.getChildren().add(login_button);
         grid.add(login_button_hbox, 1, 4);
         
-        Scene scene = new Scene(grid, 350, 270);
+        login_button.setOnAction(e-> primaryStage.setScene(app()));
         
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Login");
-        primaryStage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
+        Scene login = new Scene(grid, 350, 270);
+        
+        return login;
+}
+   private Scene app(){
+       GridPane grid = new GridPane();
+        grid.setAlignment(Pos.TOP_CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        
+        Label company = new Label("Test Company");
+        grid.add(company, 0, 0);
+        
+        Label updinter = new Label("Test interval");
+        grid.add(updinter, 1, 0);
+        
+        Label servname = new Label("Test service");
+        grid.add(servname, 2, 0);
+       
+        Scene app = new Scene(grid,350,270);
+        
+       return app;
+   }
+    
     public static void main(String[] args) {
         launch(args);
     }
