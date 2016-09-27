1e5c565b9813f7a148b3861ba976d7c8ba07e494
Maxarn <max.arnstrom@gmail.com>
2016-09-27 09:32:15 +0200

    Adjusted gitignore

--- LogAggregatorClient/src/logaggregatorclient/Connections.java
+++ LogAggregatorClient/src/logaggregatorclient/Connections.java
@@ -1,5 +1,14 @@
 package logaggregatorclient;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+
 public class Connections {
     
+    public void post() throws Exception {
+    }
 }
